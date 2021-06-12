f79efce5ed7b7bde44aca91a52bc2af0f95a9348
shristibansal <shristibansal6@gmail.com>
2021-06-12 19:10:37 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+import React, {useState} from 'react';
 
 function App() {
+  const onDelete = (todo)=>{
+    console.log("I am onDelete of todo", todo);
+    //let index = todos.indexOf(todo);
+    //todos.splice(index,1);
+    setTodos(todos.filter((e)=>{
+    return e!==todo;
+    }));
+  }
+  const [todos, setTodos] = useState([
+  {
+    sno: 1,
+    title: "Go to the market",
+    desc: "You need to go to the market to get this job done."
+  },
+  {
+    sno: 2,
+    title: "Go to the mall",
+    desc: "You need to go to the mall to get this job done."
+  },
+  {
+    sno: 3,
+    title: "Go to the park",
+    desc: "You need to go to the park to get this job done."
+  },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title = "My Todos List" searchBar = {true}/>
+      <Todos todos = {todos} onDelete={onDelete}/>
+      <Footer/>
+    </>
   );
 }
 
